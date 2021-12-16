490773eac6d5ce78838c997be2cf075db6b5bc64
Curtis L. Olson <olson126@umn.edu>
2021-12-16 11:45:59 -0600

    Shared constants.py module

---
+++ constants.py
@@ -0,0 +1,7 @@
+import math
+
+d2r = math.pi / 180
+r2d = 180 / math.pi
+
+kt2mps = 0.5144444444444444444
+mps2kt = 1.0 / kt2mps
--- sim.py
+++ sim.py
@@ -6,11 +6,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 import quaternion
-
-d2r = math.pi / 180
-r2d = 180 / math.pi
-kt2mps = 0.5144444444444444444
-mps2kt = 1.0 / kt2mps
+from constants import r2d
 
 class Simulator():
     def __init__(self):
