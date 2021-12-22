dbb487606d789d619626ae8985b282fcc5d0d085
Curtis L. Olson <curtolson@flightgear.org>
2021-12-22 13:17:03 -0600

    Add a run in 'realtime' option.

--- run_sim.py
+++ run_sim.py
@@ -10,20 +10,36 @@ Engineering and Mechanics, UAV Lab.
 """
 
 import argparse
+import time
+from apscheduler.schedulers.background import BackgroundScheduler # dnf install python3-APScheduler
+
+
 from simulator import Simulator
 
 # command line arguments
 parser = argparse.ArgumentParser(description="simulation front end")
 parser.add_argument("model", help="flight model")
+parser.add_argument('--realtime', action='store_true', help='run sim in realtime')
 args = parser.parse_args()
+
+run_time = 120
 
 sim = Simulator()
 sim.load(args.model)
 sim.reset()
-sim.trim(15)
+sim.trim(20)
 
-# run the sim with fixed inputs for a number of seconds.
-while sim.time < 120:
+def update():
     sim.update()
+
+if args.realtime:
+    sched = BackgroundScheduler()
+    sched.add_job(update, 'interval', seconds=sim.dt)
+    sched.start()
+    time.sleep(run_time)
+    sched.shutdown()
+else:
+    while sim.time <= run_time:
+        update()
 
 sim.plot()
