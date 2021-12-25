e4eb088527a64ecfca7944115886698e2fe9f5fc
Curtis L. Olson <curtolson@flightgear.org>
2021-12-25 16:01:49 -0600

    Initial revision of FlightGear interface.

---
+++ fgfs.py
@@ -0,0 +1,27 @@
+# FlightGear network interface
+
+import navpy
+import socket
+import struct
+
+from constants import mps2kt, r2d
+
+lat_deg = 45.13841697
+lon_deg = -93.2101002
+altitude_m = 400.0
+
+port = 6504
+ip = "127.0.0.1"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def send_to_fgfs(sim):
+    lla = navpy.ned2lla(sim.pos_ned, lat_deg, lon_deg, altitude_m)
+    msg = struct.pack("!ddfffff",
+                      lla[0], lla[1], lla[2]/0.3048,
+                      sim.state_mgr.phi_rad*r2d,
+                      sim.state_mgr.the_rad*r2d,
+                      sim.state_mgr.psi_rad*r2d,
+                      sim.airspeed_mps*mps2kt)
+    sock.sendto(msg, (ip, port))
+    
