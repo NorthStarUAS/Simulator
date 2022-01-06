535497c9d178dcc90fae35627fb0bd4ecf964dfb
Curtis L. Olson <curtolson@flightgear.org>
2022-01-06 09:17:58 -0600

    Convert the joystick module to a real class, add support for elevator trim.
    I know axis/hat/button mapping can vary widely from platform to
    platform, but just trying to keep the code simple for now.

--- lib/joystick.py
+++ lib/joystick.py
@@ -1,54 +1,68 @@
 # a simple locally connected (pc) joystick interface
 
 have_pygame = False
-have_joystick = False
-num_axes = 0
-num_buttons = 0
-num_hats = 0
-axes = []
-buttons = []
-hats = []
-
 try:
     import pygame
     have_pygame = True
 except:
     print("pygame import failed, joystick inactive")
 
-if have_pygame:
-    pygame.init()
-    pygame.joystick.init()
-    if pygame.joystick.get_count() > 0:
-        have_joystick = True
-        print("Detected a joystick")
-    else:
-        print("no joysticks found")
+class Joystick():
+    def __init__(self):
+        # physical values
+        self.have_joystick = False
+        self.num_axes = 0
+        self.num_buttons = 0
+        self.num_hats = 0
+        self.axes = []
+        self.buttons = []
+        self.hats = []
+
+        # logical values
+        self.throttle = 0.0
+        self.aileron = 0.0
+        self.elevator = 0.0
+        self.elevator_trim = 0.0
+        self.rudder = 0.0
         
-if have_joystick:
-    j = pygame.joystick.Joystick(0)
-    j.init()
-    num_axes = j.get_numaxes()
-    axes = [0.0] * num_axes
-    num_buttons = j.get_numbuttons()
-    buttons = [0] * num_buttons
-    num_hats = j.get_numhats()
-    hats = [0] * num_hats
+        if have_pygame:
+            pygame.init()
+            pygame.joystick.init()
+            if pygame.joystick.get_count() > 0:
+                self.have_joystick = True
+                print("Detected a joystick")
+            else:
+                print("no joysticks found")
+        
+        if self.have_joystick:
+            self.j = pygame.joystick.Joystick(0)
+            self.j.init()
+            self.num_axes = self.j.get_numaxes()
+            self.axes = [0.0] * self.num_axes
+            self.num_buttons = self.j.get_numbuttons()
+            self.buttons = [0] * self.num_buttons
+            self.num_hats = self.j.get_numhats()
+            self.hats = [0] * self.num_hats
 
-def update():
-    if not have_joystick:
-        return None, None, None, None
-    pygame.event.pump()
-    for i in range(num_axes):
-        axes[i] = j.get_axis(i)
-    for i in range(num_buttons):
-        buttons[i] = j.get_button(i)
-    for i in range(num_hats):
-        hats[i] = j.get_hat(i)
-    print(axes, buttons, hats)
-    throttle = (1.0 - axes[2]) * 0.5
-    aileron = axes[0]
-    elevator = -axes[1]
-    rudder = axes[3]
-    return throttle, aileron, elevator, rudder
-
-    
+    def update(self):
+        if not self.have_joystick:
+            return
+        
+        pygame.event.pump()
+        
+        for i in range(self.num_axes):
+            self.axes[i] = self.j.get_axis(i)
+        for i in range(self.num_buttons):
+            self.buttons[i] = self.j.get_button(i)
+        for i in range(self.num_hats):
+            self.hats[i] = self.j.get_hat(i)
+        print(self.axes, self.buttons, self.hats)
+        
+        self.throttle = (1.0 - self.axes[2]) * 0.5
+        self.aileron = self.axes[0]
+        if self.num_hats:
+            self.elevator_trim += self.hats[0][1] * 0.001
+            if self.elevator_trim < -0.25: self.elevator_trim = -0.25
+            if self.elevator_trim >  0.25: self.elevator_trim =  0.25
+        self.elevator = -self.axes[1] + self.elevator_trim
+        self.rudder = self.axes[3]
