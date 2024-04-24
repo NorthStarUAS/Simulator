2ecd649c5d0f23a0c6afe9eaa59e2aa79455e9ff
Curtis L. Olson <clolson@cirrusaircraft.com>
2024-04-24 09:28:13 -0500

    Add a bunch of changes for envelope protection and to turn off integrators and attitude hold in the controllers when not flying.

--- run_jsbsim.py
+++ run_jsbsim.py
@@ -59,26 +59,34 @@ def direct_fcs():
     control_flight_node.setBool("flaps_up", inceptor_node.getBool("flaps_up"))
 
 class NotaPID():
-    def __init__(self, name, integral_gain, antiwindup, neutral_tolerance):
+    def __init__(self, name, min_hold, max_hold, integral_gain, antiwindup, neutral_tolerance):
         self.dt = 0.02
         self.name = name
         self.int_gain = integral_gain
         self.antiwindup = antiwindup
         self.tol = neutral_tolerance
         self.cmd_neutral = True
+        self.min_hold = min_hold
+        self.max_hold = max_hold
         self.hold_cmd = 0.0
         self.error_sum = 0.0
 
-    def get_ref_value(self, input_cmd, ff_cmd, min_val, max_val, hold_val):
+    def get_ref_value(self, input_cmd, ff_cmd, min_val, max_val, cur_val, is_flying):
+        if not is_flying:
+            self.hold_cmd = cur_val
         if abs(input_cmd) < self.tol:
             if not self.cmd_neutral:
                 # print("set neutral:", self.name)
-                self.hold_cmd = hold_val
+                self.hold_cmd = cur_val
                 self.cmd_neutral = True
         else:
             self.cmd_neutral = False
+        if self.hold_cmd < self.min_hold:
+            self.hold_cmd = self.min_hold
+        if self.hold_cmd > self.max_hold:
+            self.hold_cmd = self.max_hold
         if self.cmd_neutral:
-            ref_val = (self.hold_cmd - hold_val) * 0.05 + ff_cmd
+            ref_val = (self.hold_cmd - cur_val) * 0.05 + ff_cmd
             # print(self.name, ref_rate)
         else:
             ref_val = input_cmd + ff_cmd
@@ -89,12 +97,21 @@ class NotaPID():
             ref_val = min_val
         return ref_val
 
-    def integrator(self, ref_val, cur_val):
-        self.error_sum += self.int_gain * (ref_val - cur_val) * self.dt
-        if self.error_sum < -self.antiwindup: self.error_sum = -self.antiwindup
-        if self.error_sum > self.antiwindup: self.error_sum = self.antiwindup
-        # print(self.name, "ref_val: %.2f" % ref_val, "error sum: %.2f" % self.error_sum, "%s: %.2f" % (self.name, self.error_sum * self.int_gain))
+    def integrator(self, ref_val, cur_val, is_flying=False):
+        if is_flying:
+            self.error_sum += self.int_gain * (ref_val - cur_val) * self.dt
+            if self.error_sum < -self.antiwindup: self.error_sum = -self.antiwindup
+            if self.error_sum > self.antiwindup: self.error_sum = self.antiwindup
+            # print(self.name, "ref_val: %.2f" % ref_val, "error sum: %.2f" % self.error_sum, "%s: %.2f" % (self.name, self.error_sum * self.int_gain))
+        else:
+            fade_sec = 2
+            self.error_sum *= (fade_sec - self.dt) / fade_sec
         return self.error_sum
+
+# Issue 1: life is good through take off roll and rotation, but when speed
+# threshold trips and integrators turn on, that stick back pressure for holding
+# the nose up turns into a real rate command and the nose pitches up fast and
+# pilot has to push back and then center the stick.
 
 class NotaFCS():
     def __init__(self):
@@ -108,6 +125,11 @@ class NotaFCS():
         self.pitch_stick_scale = 30 * d2r
         self.yaw_stick_scale = 20
 
+        # flying vs on ground detection
+        self.on_ground_for_sure_mps = 30
+        self.flying_for_sure_mps = 40
+        self.is_flying = False
+
         # envelope protection
         self.alpha_limit_deg = 13.0
         self.bank_limit_deg = 60.0
@@ -118,9 +140,9 @@ class NotaFCS():
         self.pitch_damp_gain = 1.0
         self.yaw_damp_gain = 5.0
 
-        self.roll_helper = NotaPID("roll", integral_gain=1.0, antiwindup=0.25, neutral_tolerance=0.02)
-        self.pitch_helper = NotaPID("pitch", integral_gain=-4.0, antiwindup=0.5, neutral_tolerance=0.03)
-        self.yaw_helper = NotaPID("yaw", integral_gain=-0.01, antiwindup=0.25, neutral_tolerance=0.02)
+        self.roll_helper = NotaPID("roll", -45, 45, integral_gain=1.0, antiwindup=0.25, neutral_tolerance=0.02)
+        self.pitch_helper = NotaPID("pitch", -15, 15, integral_gain=-4.0, antiwindup=0.5, neutral_tolerance=0.03)
+        self.yaw_helper = NotaPID("yaw", -10, 10, integral_gain=-0.01, antiwindup=0.25, neutral_tolerance=0.02)
 
         self.Ainv_lat = np.array(
             [[5775.381885098956, 82.77808579660751],
@@ -142,10 +164,10 @@ class NotaFCS():
         self.throttle_cmd = inceptor_node.getFloat("throttle")
 
         airspeed_mps = vel_node.getFloat("vc_mps")
-        if airspeed_mps < 30: airspeed_mps = 30
+        if airspeed_mps < 25: airspeed_mps = 25
         self.airspeed_mps = 0.99 * self.airspeed_mps + 0.01 * airspeed_mps
         vtrue_mps = vel_node.getFloat("vtrue_mps")
-        if vtrue_mps < 30: vtrue_mps = 30
+        if vtrue_mps < 25: vtrue_mps = 25
         self.vtrue_mps = 0.99 * self.vtrue_mps + 0.01 * vtrue_mps
         rho = 1.225
         self.qbar = 0.5 * self.airspeed_mps**2 * rho
@@ -163,28 +185,43 @@ class NotaFCS():
         self.gbody_z = cos(self.phi_deg*d2r) * cos(self.theta_deg*d2r) * gravity
         self.q_term1 = sin(self.phi_deg*d2r) * (sin(self.phi_deg*d2r) / cos(self.phi_deg*d2r)) / self.airspeed_mps
 
-        if False:
-            # sensed directly (or from sim model)
-            self.alpha_deg = aero_node.getFloat("alpha_deg")
-            self.beta_deg = aero_node.getFloat("beta_deg")
-        else:
-            # inertial+airdata estimate (behaves very wrong at low airspeeds, ok in flight!)
-            self.alpha_deg = self.alpha_func()
-            self.beta_deg = self.beta_func()
+        # flying?
+        if self.airspeed_mps < self.on_ground_for_sure_mps:
+            self.is_flying = False
+        if self.airspeed_mps > self.flying_for_sure_mps:
+            self.is_flying = True
+        print("flying:", "%.1f" % self.airspeed_mps, self.is_flying)
 
-        # Feed forward steady state q and r basd on bank angle/turn rate.
-        # Presuming a steady state level turn, compute turn rate =
-        # func(velocity, bank angle).  This is the one feed forward term used in
-        # this set of control laws and it is purely physics based and
-        # works for all fixed wing aircraft.
-        if abs(self.phi_deg) < 89:
-            turn_rate_rps = tan(self.phi_deg*d2r) * -gravity / vtrue_mps
+        if self.is_flying:
+            if True:
+                # sensed directly (or from sim model)
+                self.alpha_deg = aero_node.getFloat("alpha_deg")
+                self.beta_deg = aero_node.getFloat("beta_deg")
+            else:
+                # inertial+airdata estimate (behaves very wrong at low airspeeds, ok in flight!)
+                self.alpha_deg = self.alpha_func()
+                self.beta_deg = self.beta_func()  # this functions drifts and can get stuck!
         else:
-            turn_rate_rps = 0
-        # compute a baseline q and r for the presumed steady state level turn, this is what we dampen towards
-        baseline_q = sin(self.phi_deg*d2r) * turn_rate_rps
-        baseline_r = cos(self.phi_deg*d2r) * turn_rate_rps
-        # print("tr: %.3f" % turn_rate_rps, "q: %.3f %.3f" % (baseline_q, self.q), "r: %.3f %.3f" % (baseline_r, self.r))
+            self.alpha_deg = self.theta_deg
+            self.beta_deg = 0
+
+        if self.is_flying:
+            # Feed forward steady state q and r basd on bank angle/turn rate.
+            # Presuming a steady state level turn, compute turn rate =
+            # func(velocity, bank angle).  This is the one feed forward term used in
+            # this set of control laws and it is purely physics based and
+            # works for all fixed wing aircraft.
+            if abs(self.phi_deg) < 89:
+                turn_rate_rps = tan(self.phi_deg*d2r) * -gravity / vtrue_mps
+            else:
+                turn_rate_rps = 0
+            # compute a baseline q and r for the presumed steady state level turn, this is what we dampen towards
+            baseline_q = sin(self.phi_deg*d2r) * turn_rate_rps
+            baseline_r = cos(self.phi_deg*d2r) * turn_rate_rps
+            # print("tr: %.3f" % turn_rate_rps, "q: %.3f %.3f" % (baseline_q, self.q), "r: %.3f %.3f" % (baseline_r, self.r))
+        else:
+            baseline_q = 0
+            baseline_r = 0
 
         # Pilot commands
         roll_rate_cmd = inceptor_node.getFloat("aileron") * self.roll_stick_scale
@@ -192,17 +229,21 @@ class NotaFCS():
         beta_deg_cmd = -inceptor_node.getFloat("rudder") * self.yaw_stick_scale
 
         # envelope protection (needs to move after or into the controller or at
-        # least incorporate the ff term (and dampers?))
+        # least incorporate the ff term (and dampers?))  This must consider more
+        # than just pitch rate and may need to lower the pitch angle hold value
+        # simultaneously, however it takes time for speed to build up and alpha
+        # to come down so how/where should the limited 'hold' value get set to?
         max_q = (self.alpha_limit_deg - self.alpha_deg) * d2r * 2
         # min_q = (self.airspeed_mps - self.vne_mps) * 0.1
 
+        # bank angle limits
         max_p = (self.bank_limit_deg - self.phi_deg) * d2r * 0.5
         min_p = (-self.bank_limit_deg - self.phi_deg) * d2r * 0.5
 
         # Condition and limit the pilot requests
-        ref_p = self.roll_helper.get_ref_value(roll_rate_cmd, 0, min_p, max_p, self.phi_deg)
-        ref_q = self.pitch_helper.get_ref_value(pitch_rate_cmd, baseline_q, None, max_q, self.theta_deg)
-        ref_beta = self.yaw_helper.get_ref_value(beta_deg_cmd, 0, None, None, 0)
+        ref_p = self.roll_helper.get_ref_value(roll_rate_cmd, 0, min_p, max_p, self.phi_deg, self.is_flying)
+        ref_q = self.pitch_helper.get_ref_value(pitch_rate_cmd, baseline_q, None, max_q, self.theta_deg, self.is_flying)
+        ref_beta = self.yaw_helper.get_ref_value(beta_deg_cmd, 0, None, None, 0, self.is_flying)
 
         # compute the direct surface position to achieve the command (these
         # functions are fit from the original flight data and involve a matrix
@@ -216,9 +257,9 @@ class NotaFCS():
         # the real aircraft. Imperfect models can be due to linear fit limits,
         # change in aircraft weight and balance, change in atmospheric
         # conditions, etc.
-        aileron_int = self.roll_helper.integrator(ref_p, self.p)
-        elevator_int = self.pitch_helper.integrator(ref_q, self.q)
-        rudder_int = self.yaw_helper.integrator(ref_beta, self.beta_deg)
+        aileron_int = self.roll_helper.integrator(ref_p, self.p, self.is_flying)
+        elevator_int = self.pitch_helper.integrator(ref_q, self.q, self.is_flying)
+        rudder_int = self.yaw_helper.integrator(ref_beta, self.beta_deg, self.is_flying)
         print("integrators: %.2f %.2f %.2f" % (aileron_int, elevator_int, rudder_int))
 
         # dampers, these can be tuned to pilot preference for lighter finger tip
