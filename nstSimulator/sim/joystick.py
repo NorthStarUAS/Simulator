7515c99a08174bd5287f6dba5849281a4097e86d
Curtis L. Olson <curtolson@flightgear.org>
2025-02-12 17:15:00 -0600

    simplify and generalize the joystick code a bit and make the setup more configurable.

--- nstSimulator/sim/joystick.py
+++ nstSimulator/sim/joystick.py
@@ -19,18 +19,7 @@ from .lib.props import inceptors_node
 
 class Joystick():
     def __init__(self, joysticks_config=None):
-        # physical values
-        self.num_joysticks = 0
-        self.joys = []
-        self.mapping = {
-            "roll": [None] * 3,
-            "pitch": [None] * 3,
-            "pitch_trim": [None] * 3,
-            "yaw": [None] * 3,
-            "power": [None] * 3,
-            "flaps": [None] * 3,
-            "gear": [None] * 3,
-        }
+        self.mapping = {}
 
         # internally tracked trim values
         self.yaw_trim = 0.0
@@ -41,8 +30,7 @@ class Joystick():
 
         pygame.init()
         pygame.joystick.init()
-        self.num_joysticks = pygame.joystick.get_count()
-        if not self.num_joysticks:
+        if not pygame.joystick.get_count():
             for i in range(10):
                 print("no joysticks found")
                 sleep(0.1)
@@ -52,30 +40,17 @@ class Joystick():
         # load joysticks config
         if joysticks_config is None:
             joysticks_path = Path(__file__).parent / "joysticks.json"
-            print("loading joystick config file:", joysticks_config)
+            print("loading joystick config file:", joysticks_path)
             f = open(joysticks_path, "r")
             joysticks_config = json.load(f)
 
-        for i in range(self.num_joysticks):
+        for i in range(pygame.joystick.get_count()):
             name = pygame.joystick.Joystick(i).get_name()
-            handle = pygame.joystick.Joystick(i)
-            print("  name:", name)
-
-            joy = {}
-            joy["name"] = name
-            joy["handle"] = handle
-            joy["handle"].init()
-            joy["num_axes"] = handle.get_numaxes()
-            joy["axes"] = [0.0] * joy["num_axes"]
-            joy["num_buttons"] = handle.get_numbuttons()
-            joy["buttons"] = [0] * joy["num_buttons"]
-            joy["num_hats"] = handle.get_numhats()
-            joy["hats"] = [0] * joy["num_hats"]
-            self.joys.append(joy)
+            print("Joystick:", name, "- checking for a config:")
 
             if name in joysticks_config:
                 for key in joysticks_config[name]:
-                    print("   ", key, joysticks_config[name][key])
+                    print(" ", key, joysticks_config[name][key])
                     vals = joysticks_config[name][key]
                     if len(vals) == 2:
                         self.mapping[key] = [ vals[0], i, vals[1] ]
@@ -84,7 +59,6 @@ class Joystick():
                     else:
                         print("wrong len of vals")
 
-        print("Joystick structures:", self.joys)
         print("Joystick mapping:", self.mapping)
 
     def deadband(self, x, val):
@@ -104,37 +78,8 @@ class Joystick():
         # print(x, val, x * exp(abs(val*x))/exp(val))
         return x * exp(abs(val*x))/exp(val)
 
-    def get_input_value(self, name):
-        val = 0.0
-        if name in self.mapping:
-            mapping = self.mapping[name]
-            source = mapping[0]
-            joy_num = mapping[1]
-            element_num = mapping[2]
-            if source is not None:
-                if source == "axis":
-                    val = self.joys[joy_num]["axes"][element_num]
-                    if len(mapping) > 3:
-                        options = mapping[3]
-                        if "deadband" in options:
-                            val = self.deadband(val, options["deadband"])
-                        if "expo" in options:
-                            val = self.expo(val, options["expo"])
-                elif source == "hat":
-                    sub_num = mapping[3]
-                    val = self.joys[joy_num]["hats"][element_num][sub_num]
-                elif source == "button":
-                    val = self.joys[joy_num]["buttons"][element_num]
-                # if name == "pitch_trim_up":
-                #     print(mapping, val)
-        return val
-
-    def update(self):
-        if not self.num_joysticks:
-            return
-
-        pygame.event.pump()
-
+    # this needs to get rewritten to access the data directly
+    def debug(self):
         # read all the raw input data
         for joy in self.joys:
             handle = joy["handle"]
@@ -148,23 +93,45 @@ class Joystick():
                 joy["hats"][i] = handle.get_hat(i)
             # print(joy)
 
-        inceptors_node.setDouble("power", (1.0 - self.get_input_value("power")) * 0.5)
+    def update(self):
+        if not pygame.joystick.get_count():
+            return
 
-        inceptors_node.setDouble("roll", self.get_input_value("roll"))
+        pygame.event.pump()
+
+
+        for name in self.mapping.keys():
+            if len(self.mapping[name]) == 3:
+                [input_type, joy_num, element_num] = self.mapping[name]
+                filters = {}
+            elif len(self.mapping[name]) == 4:
+                [input_type, joy_num, element_num, filters] = self.mapping[name]
+            handle = pygame.joystick.Joystick(joy_num)
+            if input_type == "axis":
+                value = handle.get_axis(element_num)
+                if "deadband" in filters:
+                    value = self.deadband(value, filters["deadband"])
+                if "expo" in filters:
+                    value = self.expo(value, filters["expo"])
+                if "reverse" in filters:
+                    value = -value
+                if "normalize_0_1" in filters:
+                    # print("raw power:", value)
+                    value = (value + 1) * 0.5
+                inceptors_node.setDouble(name, value)
+            elif input_type == "button":
+                value = handle.get_button(element_num)
+                inceptors_node.setBool(name, value)
+            elif input_type == "hat":
+                value = handle.get_hat(element_num)
+                inceptors_node.setBool(name, value)
+            # print("  name:", name, "val:", value)
 
         trim_cmd = 0
-        trim_cmd += self.get_input_value("pitch_trim_down")
-        trim_cmd -= self.get_input_value("pitch_trim_up")
-        # trim_cmd += self.get_input_value("pitch_trim")
+        trim_cmd += inceptors_node.getDouble("pitch_trim_down")
+        trim_cmd -= inceptors_node.getDouble("pitch_trim_up")
         self.pitch_trim += 0.001 * trim_cmd
         if self.pitch_trim < -0.25: self.pitch_trim = -0.25
         if self.pitch_trim > 0.25: self.pitch_trim = 0.25
         # print("pitch trim:", self.pitch_trim)
         inceptors_node.setDouble("pitch_trim", self.pitch_trim)
-
-        inceptors_node.setDouble("pitch", -self.get_input_value("pitch"))
-
-        inceptors_node.setDouble("yaw", self.get_input_value("yaw"))
-
-        inceptors_node.setBool("flaps_down", self.get_input_value("flaps_down"))
-        inceptors_node.setBool("flaps_up", self.get_input_value("flaps_up"))
