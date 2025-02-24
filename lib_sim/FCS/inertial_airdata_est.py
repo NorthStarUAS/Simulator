759f177dee2bbac317984f5237db4bf7b0bc7778
Curtis L. Olson <curtolson@flightgear.org>
2025-02-24 06:33:19 -0600

    setup to interpolate between flap positions to hopefully minimize transients, but there is a transient in there somewhere I need to track down.

--- lib_sim/FCS/inertial_airdata_est.py
+++ lib_sim/FCS/inertial_airdata_est.py
@@ -5,30 +5,43 @@ def alpha_func(flaps_norm, qbar, az_mps2):
     # qbar = 0.5 * vc_mps**2 * 1.225_kg_m3
     if qbar < 1:
         return 0                                           # no airspeed
-    elif flaps_norm <= 0.25:
-        A = np.array( [[ -2.75733057, -1150.65361849 ]] )  # flaps up
-    elif flaps_norm <= 0.75:
-        A = np.array( [[ -4.82827705,  -956.7511148  ]] )  # flaps 50%
-    else:
-        A = np.array( [[ -8.32787384,  -966.11862015 ]] )  # flaps 100%
-    x = np.array([1, az_mps2/qbar])
-    y = A @ x
-    return y[0]
 
-# a simple alpha estimator fit from flight test data
+    x = np.array([1, az_mps2/qbar])
+
+    A0 = np.array( [[ -2.75733057, -1150.65361849 ]] )  # flaps up
+    A50 = np.array( [[ -4.82827705,  -956.7511148  ]] )  # flaps 50%
+    A100 = np.array( [[ -8.32787384,  -966.11862015 ]] )  # flaps 100%
+
+    y0 = A0 @ x
+    y50 = A50 @ x
+    y100 = A100 @ x
+
+    xp = [0, 0.5, 1.0]
+    fp = [y0[0], y50[0], y100[0]]
+
+    return np.interp(flaps_norm, xp, fp)
+
+# a inverse alpha estimator fit from flight test data, given a steady state
+# alpha and qbar, estimate the corresponding az (load factor)
 def inv_alpha_func(flaps_norm, alpha_deg, qbar):
     # qbar = 0.5 * vc_mps**2 * 1.225_kg_m3
     if qbar < 1:
         return 0                                      # no airspeed
-    elif flaps_norm <= 0.25:
-        A = np.array( [[-0.00264206, -0.00061865]] )  # flaps up
-    elif flaps_norm <= 0.75:
-        A = np.array( [[-0.00483375, -0.00071729]] )  # flaps 50%
-    else:
-        A = np.array( [[-0.00873206, -0.00071212]] )  # flaps 100%
+
     x = np.array([qbar, alpha_deg*qbar])
-    y = A @ x
-    return y[0]
+
+    A0 = np.array( [[-0.00264206, -0.00061865]] )  # flaps up
+    A50 = np.array( [[-0.00483375, -0.00071729]] )  # flaps 50%
+    A100 = np.array( [[-0.00873206, -0.00071212]] )  # flaps 100%
+
+    y0 = A0 @ x
+    y50 = A50 @ x
+    y100 = A100 @ x
+
+    xp = [0, 0.5, 1.0]
+    fp = [y0[0], y50[0], y100[0]]
+
+    return np.interp(flaps_norm, xp, fp)
 
 # a simple beta estimator fit from flight test data (todo: fit a beta function without rudder)
 def beta_func(qbar, ay, r, rudder_cmd, throttle_cmd):
