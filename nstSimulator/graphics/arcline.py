b2f88b0d30d13ce0ff4584af56f2ccea41f1f959
Curtis L. Olson <clolson@cirrusaircraft.com>
2025-08-28 11:31:48 -0500

    Add support for individual tics (called marks in the function parameters) to allow for situations where we aren't sweeping a set of tics across the entire arc.

--- nstSimulator/graphics/arcline.py
+++ nstSimulator/graphics/arcline.py
@@ -6,7 +6,7 @@ from nstSimulator.graphics.fonts import get_B612_font
 from .arc import gen_arc, gen_arc_list
 
 class ArcLine2d():
-    def __init__(self, color4=(0.9, 0.1, 0.1, 0.7), radius=1, width=1, start_deg=0, end_deg=360, steps=10, font_scale=(0.1, 0.3), tic_list=[], label_radius=1, label_list=[]):
+    def __init__(self, color4=(0.9, 0.1, 0.1, 0.7), radius=1, width=1, start_deg=0, end_deg=360, steps=10, font_scale=(0.1, 0.3), tic_list=[], mark_list=[], label_radius=1, label_list=[]):
         self.group_node = NodePath("group")
         self.group_node.setLightOff(1)
 
@@ -24,15 +24,24 @@ class ArcLine2d():
             else:
                 ls.drawTo(x, y, 0)
 
+        # generate sets of tics
         for [tic_steps, tic_scale] in tic_list:
             tic_outer = gen_arc(radius, start_deg, end_deg, tic_steps)
             tic_inner = gen_arc(radius*tic_scale, start_deg, end_deg, tic_steps)
-
             for i in range(len(tic_outer)):
                 (x1, y1) = tic_outer[i]
                 (x2, y2) = tic_inner[i]
                 ls.moveTo(x1, y1, 0)
                 ls.drawTo(x2, y2, 0)
+
+        # generate individual tics
+        for [tic_angle, tic_scale] in mark_list:
+            tic_outer = gen_arc_list(radius, [tic_angle])
+            tic_inner = gen_arc_list(radius*tic_scale, [tic_angle])
+            (x1, y1) = tic_outer[0]
+            (x2, y2) = tic_inner[0]
+            ls.moveTo(x1, y1, 0)
+            ls.drawTo(x2, y2, 0)
 
         line_node = NodePath(ls.create())
         line_node.setAntialias(AntialiasAttrib.MLine)
