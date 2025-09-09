7bb2f047b847f1e7adf5610b7e860639d0fe00a3
Curtis L. Olson <clolson@cirrusaircraft.com>
2025-09-09 13:54:36 -0500

    add support for a direct 3d polygon

--- nstSimulator/graphics/polygon.py
+++ nstSimulator/graphics/polygon.py
@@ -34,6 +34,40 @@ class Polygon2d(object):
         node.addGeom(geom)
         return node
 
+class Polygon3d():
+    def __init__(self, vertices=[], color=[1, 1, 1, 1]):
+        # make local copies
+        self.vertices = list(vertices)
+        self.color = list(color)
+
+    def makeNode(self, trangulation_plane="xy"):
+        vt = self.vertices
+        vc = self.color
+        t = Triangulator()
+        format = GeomVertexFormat.getV3c4()
+        vdata = GeomVertexData('name', format, Geom.UHStatic)
+        vertex = GeomVertexWriter(vdata, 'vertex')
+        color = GeomVertexWriter(vdata, 'color')
+        for x,y,z in vt:
+            if trangulation_plane == "xy":
+                t.addPolygonVertex(t.addVertex(x,y))
+            elif trangulation_plane == "xz":
+                t.addPolygonVertex(t.addVertex(x,z))
+            elif trangulation_plane == "yz":
+                t.addPolygonVertex(t.addVertex(y,z))
+            vertex.addData3(x, y, z)
+            color.addData4(vc[0], vc[1], vc[2], vc[3])
+        t.triangulate()
+        prim = GeomTriangles(Geom.UHStatic)
+        for n in range(t.getNumTriangles()):
+            prim.addVertices(t.getTriangleV0(n), t.getTriangleV1(n), t.getTriangleV2(n))
+        prim.closePrimitive()
+        geom = Geom(vdata)
+        geom.addPrimitive(prim)
+        node = GeomNode('gnode')
+        node.addGeom(geom)
+        return node
+
 # simple demonstration
 # poly=Polygon([(0,0),(0,1),(1,1),(1,0)])
 # import direct.directbase.DirectStart
