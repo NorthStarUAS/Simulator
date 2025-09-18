1cd4753499fe1e27f0ac60c3edd372f758c165fa
Curtis L. Olson <clolson@cirrusaircraft.com>
2025-09-18 15:21:35 -0500

    add the f16-style pitch ladder

--- nstSimulator/panel/panel.py
+++ nstSimulator/panel/panel.py
@@ -1,8 +1,11 @@
 from .guage import Guage
+from .pitch import PitchLadderF16
 
 class Panel():
     def __init__(self):
         self.guage = Guage(0.25, 0, 0)
+        self.pitch = PitchLadderF16()
 
-    def update(self):
+    def update(self, nedpos):
         self.guage.update()
+        self.pitch.update(nedpos)
