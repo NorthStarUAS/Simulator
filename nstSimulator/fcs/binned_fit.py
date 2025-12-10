67107c85d4e399c140543bc230bad5e1dc98113e
Curtis L. Olson <clolson@cirrusaircraft.com>
2025-12-10 08:55:24 -0600

    Working on multi-independent variable binned fitting.

--- nstSimulator/fcs/binned_fit.py
+++ nstSimulator/fcs/binned_fit.py
@@ -24,29 +24,49 @@ from math import ceil, floor, log10
 import numpy as np
 
 class AverageBin():
-    def __init__(self, x_val, tf=10):
-        self.x_val = x_val
+    def __init__(self, x_vals=[], tf=10):
+        if type(x_vals) is not list:
+            x_vals = [x_vals]
+
+        # bin
+        self.x_vals = x_vals
         self.tf = tf
-        self.count = 0
-        self.y_sum = 0
-        self.y_avg = 0
         self.y_filt = 0
         self.xp = []
         self.fp = []
 
+        # welford (Knuth) mean/var
+        self.k = 0
+        self.M = 0
+        self.S = 0
+        self.var = 0
+
+    # Numerically robust incremental mean and variance (Knuth)
+    # From: https://www.embeddedrelated.com/showarticle/785.php
+    def welford(self, x):
+        self.k += 1
+        Mnext = self.M + (x - self.M) / self.k
+        self.S = self.S + (x - self.M)*(x - Mnext)
+        self.M = Mnext
+        if self.k > 1:
+            self.var = self.S/(self.k-1)
+        else:
+            self.var = 0
+
     def AddData(self, y, dt):
-        if self.count == 0:
+        self.welford(y)
+
+        if self.k == 1:
             self.y_filt = y
-        self.count += 1
-        self.y_sum += y
-        self.y_avg = self.y_sum / self.count
-        tf = self.count * dt
+        # self.y_sum += y
+        # self.y_avg = self.y_sum / self.count
+        tf = self.k * dt
         if tf > self.tf:
             tf = self.tf
         w = dt / tf
         self.y_filt = (1-w)*self.y_filt + w*y
 
-class IntervalBinnedFit():
+class IntervalBinnedFit1d():
     def __init__(self, bin_width):
         self.bin_width = bin_width
         self.half_bin_width = bin_width * 0.5
@@ -66,7 +86,7 @@ class IntervalBinnedFit():
         # print("%.6f" % key)
         self.total_count += 1
         if not key in self.bins:
-            self.bins[key] = AverageBin(key)
+            self.bins[key] = AverageBin([key])
         self.bins[key].AddData(y, dt)
         # print("x:", x, "key:", key)
 
@@ -75,15 +95,80 @@ class IntervalBinnedFit():
         self.fp = []
         self.w = []
         for bin in self.bins.values():
-            self.xp.append(bin.x_val)
-            self.fp.append(bin.y_avg)
-            self.w.append(bin.count / self.total_count)
+            self.xp.append(bin.x_vals[0])
+            self.fp.append(bin.M)
+            self.w.append(bin.k / self.total_count)
         if len(self.xp):
             self.fit_model = np.polyfit(self.xp, self.fp, deg=1, w=self.w)
             # print("polyfit:", self.fit_model)
 
     def Interp(self, x):
         return self.fit_model[0]*x + self.fit_model[1]
+
+class IntervalBinnedFit():
+    def __init__(self, bin_widths=[]):
+        self.dim = len(bin_widths)
+        self.bin_widths = []
+        self.half_bin_widths = []
+        self.bin_rounds = []
+
+        for bin_width in bin_widths:
+            self.bin_widths.append(bin_width)
+            self.half_bin_widths.append(bin_width * 0.5)
+            br = ceil(-log10(bin_width)) + 1  # compute number of sig digits of cell size (+ 1) for bin naming
+            if br < 0: br = 0
+            self.bin_rounds.append(br)
+        self.total_count = 0
+        self.bins = {}
+        self.fit_model = None
+        self.xp = []
+        self.fp = []
+
+    def AddData(self, xs=[], y=0, dt=0.01):
+        # compute bin
+        # print("%.3f" % (((x - self.minx) / self.range) * self.num_bins))
+        key=""
+        ks = []
+        for i in range(self.dim):
+            if i > 0:
+                key += ","
+            k = ( round( floor(xs[i] / self.bin_widths[i]) * self.bin_widths[i] + self.half_bin_widths[i], self.bin_rounds[i] ) )
+            ks.append(k)
+            key += str(k)
+            print("kk:", str(ks))
+        print("key:", key)
+        self.total_count += 1
+        if not key in self.bins:
+            self.bins[key] = AverageBin(ks)
+        self.bins[key].AddData(y, dt)
+        # print("x:", x, "key:", key)
+
+    def FitModel(self):
+        if len(self.bins) < self.dim:
+            print("not enough bins to fit model")
+            return
+        self.xp = []
+        self.fp = []
+        self.w = []
+        # print("bins:", self.bins)
+        for bin in self.bins.values():
+            self.xp.append(bin.x_vals + [1])
+            self.fp.append(bin.M)
+            self.w.append(bin.k / self.total_count)
+        # print("xp:", self.xp)
+        result = np.linalg.lstsq(self.xp, self.fp, rcond=None)
+        self.fit_model = result[0]
+        # print("fit_model:", self.fit_model)
+
+    def Interp(self, xs):
+        if len(xs) != self.dim or self.fit_model is None:
+            print("Interp: wrong dimension:", len(xs), "expected:", self.dim)
+            return 0
+        result = 0
+        for i in range(self.dim):
+            result += self.fit_model[i]*xs[i]
+        result += self.fit_model[-1]
+        return result
 
 class old_RangeBinnedFit():
     def __init__(self, minx, maxx, num_bins):
@@ -127,7 +212,7 @@ class old_RangeBinnedFit():
             for i in range(len(self.xp)):
                 f.write("%.3f %.3f\n" % (self.xp[i], self.fp[i]))
             f.close()
-            self.fit_model = np.polyfit(self.xp, self.fp, deg=1, w=self.w)
+            self.fit_model = np.polyfit(self.xp, self.fp, deg=1, w=self.w)[0]
             print("polyfit:", self.fit_model)
 
     def Interp(self, x):
@@ -140,3 +225,68 @@ class old_RangeBinnedFit():
 # for i in np.linspace(-1, 1, 37):
 #     # print(i, np.interp(i, a.xp, a.fp))
 #     print(i, a.AddData(i, 0))
+
+if __name__ == "__main__":
+    from math import sqrt
+    import random
+    import matplotlib.pyplot as plt
+
+    if True:
+        a = IntervalBinnedFit1d(0.5)
+
+        for i in range(1000):
+            x = random.uniform(-10, 10)
+            y = 2.0*x + 3.0 + random.gauss(0, 5.0)
+            a.AddData(x, y, 0.1)
+
+        a.FitModel()
+
+        xs = []
+        ys = []
+        ys_fit = []
+        err = []
+        for x_vals, bin in sorted(a.bins.items()):
+            print("bin xs:", x_vals, "M:", bin.M, "var:", bin.var, "k:", bin.k)
+            xs.append(x_vals)
+            ys.append(bin.M)
+            err.append(sqrt(bin.var))
+            ys_fit.append(a.Interp(x_vals))
+
+        # plt.plot(xs, ys, label="true")
+        plt.plot(xs, ys_fit, label="binned fit")
+        plt.errorbar(xs, ys, err, color='red', fmt='o', label="binned averages")
+        plt.grid()
+        plt.legend()
+
+        plt.show()
+
+    if True:
+        b = IntervalBinnedFit([0.01, 0.5, 10])
+        for i in range(1000):
+            x1 = random.uniform(-0.2, 0.2)
+            x2 = random.uniform(-10, 10)
+            x3 = random.uniform(-100, 100)
+
+            y = 2.0*x1 + 3.0*x2 + 4.0*x3 + random.gauss(0, 1.0)
+            b.AddData([x1, x2, x3], y, 0.1)
+            b.FitModel()
+            print("y: %.1f y_fit %.1f  diff: %.2f" % (y, b.Interp([x1, x2, x3]), y - b.Interp([x1, x2, x3])))
+
+        # xs = []
+        # ys = []
+        # ys_fit = []
+        # err = []
+        # for x_vals, bin in sorted(b.bins.items()):
+        #     print("bin xs:", x_vals, "M:", bin.M, "var:", bin.var, "k:", bin.k)
+        #     xs.append(x_vals)
+        #     ys.append(bin.M)
+        #     err.append(sqrt(bin.var))
+        #     ys_fit.append(a.Interp(x_vals))
+
+        # # plt.plot(xs, ys, label="true")
+        # plt.plot(xs, ys_fit, label="binned fit")
+        # plt.errorbar(xs, ys, err, color='red', fmt='o', label="binned averages")
+        # plt.grid()
+        # plt.legend()
+
+        # plt.show()
